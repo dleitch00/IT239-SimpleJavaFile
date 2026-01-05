27c21a919138920fdac093a1b1db3b511d999e5f
dleitch00 <dleitch00@gmail.com>
2026-01-04 19:15:33 -0500

    Java syntax error

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello
     public static void main(String[] args) 
     {
         System.out.println("Hello, world! From: Dylan :]");
-        System.out.println("Hello, world! From: Josh :]")
+        System.out("Hello, world! From: Josh :]")
     }
 }
